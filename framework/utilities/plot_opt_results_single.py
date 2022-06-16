015dfb480f1afcf89d41cad9bc385dfc2afd69a3
Alejandro Rios <aarc.88@gmail.com>
2022-06-16 17:59:36 -0300

    nada

--- framework/utilities/plot_opt_results_single.py
+++ framework/utilities/plot_opt_results_single.py
@@ -2,25 +2,29 @@ import pickle
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-with open(r"Database/Results_Multi_Optim/functions/functions_multi_obj_DD_profit_cost.pkl", "rb") as input_file:
+with open(r"Database/Results_Multi_Optim/functions/functions_multi_obj_DD_profit_CO2.pkl", "rb") as input_file:
     df_DD = pickle.load(input_file)
 
-with open(r"Database/Results_Multi_Optim/functions/functions_multi_obj_GCD_profit_cost.pkl", "rb") as input_file:
+with open(r"Database/Results_Multi_Optim/functions/functions_multi_obj_GCD_profit_CO2.pkl", "rb") as input_file:
     df_GCD = pickle.load(input_file)
 
-with open(r"Database/Results_Multi_Optim/functions/case7_profit_pareto.pkl", "rb") as input_file:
+with open(r"Database/Results_Multi_Optim/functions/case5_profit_pareto.pkl", "rb") as input_file:
     F1_GCD = pickle.load(input_file)
-with open(r"Database/Results_Multi_Optim/functions/case7_cost_pareto.pkl", "rb") as input_file:
+with open(r"Database/Results_Multi_Optim/functions/case5_C02_pareto.pkl", "rb") as input_file:
     F2_GCD = pickle.load(input_file)
 
-with open(r"Database/Results_Multi_Optim/functions/case8_profit_pareto.pkl", "rb") as input_file:
+with open(r"Database/Results_Multi_Optim/functions/case6_profit_pareto.pkl", "rb") as input_file:
     F1_DD = pickle.load(input_file)
-with open(r"Database/Results_Multi_Optim/functions/case8_cost_pareto.pkl", "rb") as input_file:
+with open(r"Database/Results_Multi_Optim/functions/case6_C02_pareto.pkl", "rb") as input_file:
     F2_DD = pickle.load(input_file)
 
-with open(r"Database/Results_Multi_Optim/variables/vars_multi_obj_DD_profit_cost.pkl", "rb") as input_file:
+with open(r"Database/Results_Multi_Optim/variables/vars_multi_obj_GCD_profit_CO2.pkl", "rb") as input_file:
     df_vars = pickle.load(input_file)
 
+
+df_GCD = pd.DataFrame(
+    {'X1': F1_GCD,
+     'X2': F2_GCD,})
 
 print(df_DD.head())
 
@@ -31,14 +35,11 @@ min_CO2 = np.min(df_GCD['X2'])
 print('max prof',max_profit)
 print('min CO2',min_CO2)
 
+input = max_profit 
 
-input = -1571683
-print(df_DD.iloc[(df_DD['X1']-input).abs().argsort()[:2]])
-input2 = 69353
-print(df_DD.iloc[(df_DD['X2']-input2).abs().argsort()[:2]])
+print('meanProf',df_vars.iloc[(df_vars['X1']-input).abs().argsort()[:3]])
 
-
-print(df_vars.iloc[1824])
+print('vars mean cO2',df_vars.iloc[233])
 # F1_DD = pd.DataFrame(F1_DD, columns=['F1'])
 # F2_DD = pd.DataFrame(F2_DD, columns=['F2'])
 
@@ -65,10 +66,10 @@ ax.set_xlabel('Profit [US$]')
 ax.set_ylabel('CO2 efficiecy [kg/nm]')
 
 
-ax.scatter(df_GCD['X1'], df_GCD['X2'],s=30, facecolors='none', edgecolors='grey',alpha = 0.5,label='GCD solutions')
-ax.scatter(df_DD['X1'], df_DD['X2'],s=30, facecolors='none', edgecolors='skyblue',alpha = 0.5,label='DD solutions')
-ax.scatter(F1_GCD, F2_GCD, s=50, facecolors='none',marker= '^',edgecolors='black')
-ax.scatter(F1_DD, F2_DD, s=50, facecolors='none',marker= 's',edgecolors='black')
+ax.scatter(df_GCD['X1'], df_GCD['X2'],s=20, facecolors='none', edgecolors='red',alpha = 0.5,label='GCD solutions')
+# ax.scatter(df_DD['X1'], df_DD['X2'],s=30, facecolors='none', edgecolors='skyblue',alpha = 0.5,label='DD solutions')
+ax.scatter(F1_GCD, F2_GCD, s=50, facecolors='none',marker= '^',edgecolors='blue')
+# ax.scatter(F1_DD, F2_DD, s=50, facecolors='none',marker= 's',edgecolors='black')
 ax.set_title("Objective Space")
 
 # ax.scatter(df_vars['X1'], df_vars['X3'],s=30, facecolors='none', edgecolors='grey',alpha = 0.5,label='GCD solutions')
